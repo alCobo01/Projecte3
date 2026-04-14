6ec6db39ea5c1da16d3c696a3e1ef5cd74d0fb7e
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2026-04-14 18:41:22 +0200

    Fixed and improved jump behaviour

--- Assets/Scripts/Characters/Player/PlayerJumpController.cs
+++ Assets/Scripts/Characters/Player/PlayerJumpController.cs
@@ -13,7 +13,7 @@ public class PlayerJumpController : MonoBehaviour
     private JumpBehaviour _jumpBehaviour;
     private GroundCheck _groundCheck;
     private int _jumpsRemaining;
-    private bool _isJumpHeld;
+    private bool _isJumpHeld, _wasGrounded;
     
     private void Awake()
     {
@@ -21,11 +21,23 @@ public class PlayerJumpController : MonoBehaviour
         _jumpBehaviour = GetComponent<JumpBehaviour>();
         _groundCheck = GetComponent<GroundCheck>();
     }
+
+    private void Start()
+    {
+        _wasGrounded = _groundCheck.IsGrounded;
+        _jumpsRemaining = _wasGrounded ? maxJumps : 0;
+        _inputController.OnJumpEvent += HandleJump;
+    } 
     
-    private void Start() => _inputController.OnJumpEvent += HandleJump;
     private void OnDisable() => _inputController.OnJumpEvent -= HandleJump;
 
-    private void Update() { if (_groundCheck.IsGrounded) _jumpsRemaining = maxJumps; } 
+    private void Update()
+    {
+        var isGrounded = _groundCheck.IsGrounded;
+        if (isGrounded && !_wasGrounded) _jumpsRemaining = maxJumps;
+        _wasGrounded = isGrounded;
+    } 
+    
     private void FixedUpdate()
     {
         if (_isJumpHeld) _jumpBehaviour.HoldJump(Time.fixedDeltaTime);
