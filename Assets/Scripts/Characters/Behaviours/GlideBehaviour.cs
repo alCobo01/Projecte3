2274f5cdd8fb17761bd799d13b190d8965c42471
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2026-04-23 16:11:13 +0200

    gliding and jump now works yayyyy

--- Assets/Scripts/Characters/Behaviours/GlideBehaviour.cs
+++ Assets/Scripts/Characters/Behaviours/GlideBehaviour.cs
@@ -5,7 +5,7 @@ public class GlideBehaviour : MonoBehaviour, IWindAffected
     public bool IsGliding { get; set; }
     
     [SerializeField] private float glidingGravityScale = 0.5f;
-    [SerializeField] private float defaultGravityScale = 3f;
+    [SerializeField] private float defaultGravityScale = 1f;
     [SerializeField] private float maxFallSpeed = -2f;
 
     private Rigidbody2D _rb;
@@ -14,15 +14,14 @@ public class GlideBehaviour : MonoBehaviour, IWindAffected
 
     private void FixedUpdate()
     {
-        if (IsGliding)
+        if (IsGliding && _rb.linearVelocityY <= 0f)
         {
             _rb.gravityScale = glidingGravityScale;
-            if (!(_rb.linearVelocityY < 0)) return;
             var velY = Mathf.Max(_rb.linearVelocityY, maxFallSpeed);
             _rb.linearVelocity = new Vector2(_rb.linearVelocityX, velY);
         }
-        else _rb.gravityScale = defaultGravityScale;
-        
+        else
+            _rb.gravityScale = defaultGravityScale;
     }
     
     public void ApplyWindForce(Vector2 force)
